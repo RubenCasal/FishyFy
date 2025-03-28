cffcdffb9e0155b9cb9b1e214b33b2e60e24b268
RubenCasal <e.ruben.casal@gmail.com>
2025-03-28 12:08:34 +0100

    readme images

--- main.py
+++ main.py
@@ -3,23 +3,25 @@ from process_yolo_dataset import process_yolo_subset, update_yaml, get_LUT
 import os
 import time
 if __name__ == "__main__":
-    dataset_dir = "./top_view_person_dataset"
-    output_dir = "./fisheye_top_view_person_dataset"
-    subsets = ['train',  'test', 'valid']
-    strength = 0.65
+    DATASET_DIR = "./top_view_person_dataset"
+    OUTPUT_DIR = "./fisheye_top_view_person_dataset"
+    SUBSETS = ['train',  'test', 'valid']
+    FISHEYE_STRENGTH = 0.7
+
+
     start_time = time.perf_counter()
-    map_x, map_y = get_LUT(dataset_dir, strength)
-    for subset in subsets:
-        images_dir = os.path.join(dataset_dir, subset, 'images')
-        labels_dir = os.path.join(dataset_dir, subset, 'labels')
-        output_images_dir = os.path.join(output_dir, subset, 'images')
-        output_labels_dir = os.path.join(output_dir, subset, 'labels')
+    map_x, map_y = get_LUT(DATASET_DIR, FISHEYE_STRENGTH)
+    for subset in SUBSETS:
+        images_dir = os.path.join(DATASET_DIR, subset, 'images')
+        labels_dir = os.path.join(DATASET_DIR, subset, 'labels')
+        output_images_dir = os.path.join(OUTPUT_DIR, subset, 'images')
+        output_labels_dir = os.path.join(OUTPUT_DIR, subset, 'labels')
 
         process_yolo_subset(images_dir, labels_dir, output_images_dir, output_labels_dir, map_x, map_y)
 
-    input_yaml = os.path.join(dataset_dir, 'data.yaml')
-    output_yaml = os.path.join(output_dir, 'data.yaml')
-    update_yaml(input_yaml, output_yaml, output_dir)
+    input_yaml = os.path.join(DATASET_DIR, 'data.yaml')
+    output_yaml = os.path.join(OUTPUT_DIR, 'data.yaml')
+    update_yaml(input_yaml, output_yaml, OUTPUT_DIR)
 
     end_time = time.perf_counter()
     elapsed_time = end_time - start_time
